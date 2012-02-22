acfb6ed87a050ae82255a2ff7276f0cc64cf8c75
Rick Hou <rick@sweetyhigh.com>
2012-02-21 18:01:01 -0800

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eyecuelab::Application.config.secret_token = '505239a6f0e5592c2492fe81015354a604284b718263d843b7c7dc6dead30415298543baea34fb10f7df80003538ec59ce712b323673ca661657b89562dd1864'
