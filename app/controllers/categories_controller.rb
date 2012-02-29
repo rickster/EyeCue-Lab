c936749cfa54f430580e21f17125130c7f4955b9
Jim McGaw <smoochy@Jim-McGaws-MacBook-Pro.local>
2012-02-28 16:47:45 -0800

    Add DS_Store file to gitignore; fix CSS hover in navigation; allow anonymous visitors to view category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:show]
   # GET /categories
   # GET /categories.json
   def index
